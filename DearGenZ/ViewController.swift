7b5e203387dc66706da74647dd6db2c09e6a16a6
lravury <52671065+lravury@users.noreply.github.com>
2020-07-27 23:03:27 -0500

    Initial Commit

---
+++ DearGenZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DearGenZ
+//
+//  Created by Anitha Ravury on 7/27/20.
+//  Copyright © 2020 Lauren Ravury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
