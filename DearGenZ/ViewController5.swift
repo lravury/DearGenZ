a2f73701106814858b4eb7b63fab9f7be6386afa
Sophia Bowie <sophiabowiee@gmail.com>
2020-07-30 13:54:51 -0500

    button working

--- DearGenZ/ViewController5.swift
+++ DearGenZ/ViewController5.swift
@@ -20,7 +20,7 @@ class ViewController5: UIViewController {
     
 
     @IBAction func gretaButton(_ sender: Any) {
-        bioText.text = "Greta Thunberg \n Greta is a 17 year old girl from Sweden who is fighting to prevent climate change. Beginning in 2018, Greta started protesting against her country's high carbon emissions. She started a movement she called 'School Strike for Climate', and urged students around the world to skip school to protest against climate change. Since then, Greta has led many protests and even spoken at the United Nations."
+        bioText.text = "Greta Thunberg \n \n Greta is a 17 year old girl from Sweden who is fighting to prevent climate change. Beginning in 2018, Greta started protesting against her country's high carbon emissions. She started a movement she called 'School Strike for Climate', and urged students around the world to skip school to protest against climate change. Since then, Greta has led many protests and even spoken at the United Nations."
     }
     
     /*
