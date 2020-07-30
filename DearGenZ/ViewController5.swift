c612d7c8bdacf11b7e9153201e39fa90494ee704
Annelise Rasmussen <annelisecharlotte86@gmail.com>
2020-07-30 15:00:04 -0500

    annelise's important stuff

--- DearGenZ/ViewController5.swift
+++ DearGenZ/ViewController5.swift
@@ -12,15 +12,22 @@ import UIKit
 class ViewController5: UIViewController {
 
     @IBOutlet weak var bioText: UILabel!
+    @IBOutlet weak var pageTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+
     @IBAction func gretaButton(_ sender: Any) {
-        bioText.text = "Greta Thunberg \n Greta is a 17 year old girl from Sweden who is fighting to prevent climate change. Beginning in 2018, Greta started protesting against her country's high carbon emissions. She started a movement she called 'School Strike for Climate', and urged students around the world to skip school to protest against climate change. Since then, Greta has led many protests and even spoken at the United Nations."
+        bioText.text = "Greta Thunberg \n \n Greta is a 17 year old girl from Sweden who is fighting to prevent climate change. Beginning in 2018, Greta started protesting against her country's high carbon emissions. She started a movement she called 'School Strike for Climate', and urged students around the world to skip school to protest against climate change. Since then, Greta has led many protests and even spoken at the United Nations."
     }
+    
+    @IBAction func emmaButton(_ sender: Any) {
+        bioText.text = "Emma Gonzalez"
+    }
+    
     
     /*
     // MARK: - Navigation
@@ -31,5 +38,5 @@ class ViewController5: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-//test
+//  test  2
 }
