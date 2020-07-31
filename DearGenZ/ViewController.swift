2587553de0d13d9d207fbaad141e3613a7d62ec1
lravury <52671065+lravury@users.noreply.github.com>
2020-07-30 22:52:01 -0500

    new files

--- DearGenZ/ViewController.swift
+++ DearGenZ/ViewController.swift
@@ -11,10 +11,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+ @IBOutlet private var backgroundOne: UIImageView!
+      @IBOutlet private var backgroundTwo: UIImageView!
+      @IBOutlet private var backgroundThree: UIImageView!
+      @IBOutlet private var backgroundFour: UIImageView!
+      @IBOutlet private var backgroundFive: UIImageView!
+    
+    
+    @objc func one() {
+    backgroundOne.alpha = 1
+    backgroundTwo.alpha = 0
+    UIView.beginAnimations(nil, context: nil)
+    UIView.setAnimationDelegate(self)
+    UIView.setAnimationDuration(1)
+    UIView.setAnimationCurve(.easeIn)
+    backgroundTwo.alpha = 1
+    backgroundOne.alpha = 0
+    UIView.commitAnimations()
+    perform(#selector(ViewController.two), with: nil, afterDelay: 2.5)
+    }
+    @objc func two() {
+        backgroundTwo.alpha = 1
+        backgroundThree.alpha = 0
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDelegate(self)
+        UIView.setAnimationDuration(1)
+        UIView.setAnimationCurve(.easeIn)
+        backgroundThree.alpha = 1
+        backgroundTwo.alpha = 0
+        UIView.commitAnimations()
+        perform(#selector(ViewController.three), with: nil, afterDelay: 2.5)
+        
+    }
+    
+    @objc func three() {
+        backgroundThree.alpha = 1
+        backgroundFour.alpha = 0
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDelegate(self)
+        UIView.setAnimationDuration(1)
+        UIView.setAnimationCurve(.easeIn)
+        backgroundFour.alpha = 1
+        backgroundThree.alpha = 0
+        UIView.commitAnimations()
+        perform(#selector(ViewController.four), with: nil, afterDelay: 2.5)
+        
+    }
+    
+    @objc func four() {
+        backgroundFour.alpha = 1
+        backgroundFive.alpha = 0
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDelegate(self)
+        UIView.setAnimationDuration(1)
+        UIView.setAnimationCurve(.easeIn)
+        backgroundFive.alpha = 1
+        backgroundFour.alpha = 0
+        UIView.commitAnimations()
+        perform(#selector(ViewController.five), with: nil, afterDelay: 2.5)
+        
+    }
+    
+    @objc func five() {
+        backgroundFive.alpha = 1
+        backgroundOne.alpha = 0
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDelegate(self)
+        UIView.setAnimationDuration(1)
+        UIView.setAnimationCurve(.easeIn)
+        backgroundOne.alpha = 1
+        backgroundFive.alpha = 0
+        UIView.commitAnimations()
+        perform(#selector(ViewController.one), with: nil, afterDelay: 2.5)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundOne.alpha = 1
+               backgroundTwo.alpha = 0
+               backgroundThree.alpha = 0
+               backgroundFour.alpha = 0
+               backgroundFive.alpha = 0
+               perform(#selector(ViewController.one), with: nil, afterDelay: 2)
+       
     }
 
 
