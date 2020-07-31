784e712605a096a5375b547b42374806ae9b4f82
Sophia Bowie <sophiabowiee@gmail.com>
2020-07-31 09:30:20 -0500

    sophia final changes

--- DearGenZ/ViewController5.swift
+++ DearGenZ/ViewController5.swift
@@ -12,6 +12,7 @@ import UIKit
 class ViewController5: UIViewController {
 
     @IBOutlet weak var bioText: UILabel!
+    @IBOutlet weak var instaButton: UIButton!
     @IBOutlet weak var pageTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +23,23 @@ class ViewController5: UIViewController {
 
     @IBAction func gretaButton(_ sender: Any) {
         bioText.text = "Greta Thunberg \n \n Greta is a 17 year old girl from Sweden who is fighting to prevent climate change. Beginning in 2018, Greta started protesting against her country's high carbon emissions. She started a movement she called 'School Strike for Climate', and urged students around the world to skip school to protest against climate change. Since then, Greta has led many protests and even spoken at the United Nations."
+        instaButton.isHidden = false
     }
+    
     
     @IBAction func emmaButton(_ sender: Any) {
         bioText.text = "Emma Gonzalez"
+        instaButton.isHidden = true
     }
     
+    @IBAction func instaButton(_ sender: Any) {
+        if let url = URL(string: "https://www.instagram.com/gretathunberg/") {
+            UIApplication.shared.open(url, options: [:]) {
+                boolean in
+                // do something with the boolean
+            }
+        }
+    }
     
     /*
     // MARK: - Navigation
