8c4a37398cfddaa6a3594979391314c5aa60414d
lravury <52671065+lravury@users.noreply.github.com>
2020-08-05 00:30:02 -0500

    new

--- DearGenZ/ViewController5.swift
+++ DearGenZ/ViewController5.swift
@@ -12,7 +12,9 @@ import UIKit
 class ViewController5: UIViewController {
 
     @IBOutlet weak var bioText: UILabel!
+
     @IBOutlet weak var activistsPage: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,9 +24,12 @@ class ViewController5: UIViewController {
 
     @IBAction func gretaButton(_ sender: Any) {
         bioText.text = "Greta Thunberg \n \n Greta is a 17 year old girl from Sweden who is fighting to prevent climate change. Beginning in 2018, Greta started protesting against her country's high carbon emissions. She started a movement she called 'School Strike for Climate', and urged students around the world to skip school to protest against climate change. Since then, Greta has led many protests and even spoken at the United Nations."
+      
     }
     
+    
     @IBAction func emmaButton(_ sender: Any) {
+
         bioText.text = "Emma Gonzalez \n \n Emma is a 20 year old advocate for gun control. Her presence in activism began after the tragic February 2018 Stoneman Douglas High School shooting in Parkland, Florida. After this tragedy occurred Emma Gonzalez and other Parkland shooting survivors organized March for Our Lives, a country-wide movement for gun control. She began the rally with a powerful speech that lasted 6 minutes and 20 seconds, the length of time it took the shooter to shoot up Stoneman Douglas High School. Since then, she has founded Never Again MSD, a student-led committee advocating for gun control."
     }
     @IBAction func yaraButton(_ sender: Any) {
@@ -35,8 +40,18 @@ class ViewController5: UIViewController {
     }
     @IBAction func marleyButton(_ sender: Any) {
         bioText.text = "Marley Dias \n \n Marley is a 15 year old activist and feminist. When in middle school, Marley launched a campaign called #1000BlackGirlBooks with the goal to collect and donate 1,000 books that feature black girls as the lead protagonist. To this day, Marley has collected over 12,000 books. Due to her campaign’s success, Marley has spoken at many conferences such as The Forbes Women’s Summit, the Social Innovation Summit, and the White House’s United State of Women, along with several others. Currently, she is the youngest member of the Forbes 30 under 30 list. Since then, Marley has continued to inspire others through the release of her book Marley Dias Gets It Done: And So Can You!"
+        
+
     }
     
+    @IBAction func instaButton(_ sender: Any) {
+        if let url = URL(string: "https://www.instagram.com/gretathunberg/") {
+            UIApplication.shared.open(url, options: [:]) {
+                boolean in
+                // do something with the boolean
+            }
+        }
+    }
     
     /*
     // MARK: - Navigation
